8b014599e0fc7bdeb51ae7bdfb381a03a4d201eb
Artem Borisov <dedsa2002@gmail.com>
2018-02-07 17:32:21 +0300

    drivers: media: camera_v2: Import Xiaomi changes

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -17,6 +17,7 @@
 #include "camera.h"
 #include "msm_cci.h"
 #include "msm_camera_dt_util.h"
+#include <linux/hqsysfs.h>
 
 /* Logging macro */
 #undef CDBG
@@ -1023,11 +1024,20 @@ CSID_TG:
 
 	msm_sensor_fill_sensor_info(s_ctrl, probed_info, entity_name);
 
+	if (0 == s_ctrl->id) {
+		hq_regiser_hw_info(HWID_MAIN_CAM, (char *)(s_ctrl->sensordata->eeprom_name));
+	} else if (1 == s_ctrl->id) {
+		hq_regiser_hw_info(HWID_MAIN_CAM_2, (char *)(s_ctrl->sensordata->eeprom_name));
+	} else if (2 == s_ctrl->id) {
+		hq_regiser_hw_info(HWID_SUB_CAM, (char *)(s_ctrl->sensordata->eeprom_name));
+	}
+
 	/*
-	 * Set probe succeeded flag to 1 so that no other camera shall
-	 * probed on this slot
-	 */
+         * Set probe succeeded flag to 1 so that no other camera shall
+         * probed on this slot
+         */
 	s_ctrl->is_probe_succeed = 1;
+
 	return rc;
 
 camera_power_down:
